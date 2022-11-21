cb2bf08cf2c229c3e102584556e0b3639aa56d73
Anders Larsen <andersoelarsen@gmail.com>
2022-11-21 20:51:28 +0100

    added astro/image integration, added images folder and an image from the old site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact()]
+  integrations: [tailwind(), preact(), image()]
 });
