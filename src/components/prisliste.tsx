482188b7ce2c3a0ef9e21b1f0dfdccb5a1ad1424
Anders Larsen <andersoelarsen@gmail.com>
2022-12-26 20:38:58 +0100

    added new text and images to pusblished book pages.

--- src/components/prisliste.tsx
+++ src/components/prisliste.tsx
@@ -11,9 +11,9 @@ const Prisliste = () => {
       <div className='my-4'>
         <h4 className='text-lg font-semibold'>Bøker av Bryggepraten</h4>
         {booksBrygge ? booksBrygge.map(book => {return (
-          <div className='w-full flex justify-between hover:bg-slate-50'>
+          <div className='w-full grid grid-cols-[1fr,auto]  hover:bg-slate-50 gap-y-4'>
             <div>{book.tittel}</div>
-            <div>{book.pris ? `${book.pris},-` : '(ta kontakt me forfatter)'}</div>
+            <div className='text-right justify-self-end'>{book.pris ? `${book.pris},-` : '(ta kontakt med forfatter)'}</div>
           </div>
         )}) : null}
         
@@ -21,18 +21,13 @@ const Prisliste = () => {
       <hr />
       <div className='my-4'>
         <h4 className='text-lg font-semibold'>Andre forfattere</h4>
-        {booksOther ? booksOther.map(book => {return (<div className='w-full grid grid-cols-2 md:grid-cols-3 hover:bg-slate-50' >
+        {booksOther ? booksOther.map(book => {return (<div className='w-full grid grid-cols-[1fr,auto] md:grid-cols-3 hover:bg-slate-50 gap-y-4' >
             <div>{book.tittel}</div>
             <div className='hidden md:block' >av {book.forfattere}</div>
-            <div className='justify-self-end'>{book.pris ? `${book.pris},-` : '(ta kontakt me forfatter)'}</div>
+            <div className='text-right justify-self-end'>{book.pris ? `${book.pris},-` : '(ta kontakt med forfatter)'}</div>
           </div>)} ): <div>Ingen bøkær funnet</div>}
       </div>
       
-      
-      
-        
-
-
       <a href='#' className='p-2 m-2 border text-center rounded-sm w-full md:w-36 justify-self-center md:justify-self-end shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100 bg-[#efb261] text-black text-secondary hover:text-amber-100 hover:bg-yellow-900'>Bestill her</a>
     </section>
   )
