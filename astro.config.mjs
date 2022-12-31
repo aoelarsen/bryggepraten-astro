abb99d51ebd2a6f009e6a589b366c07a7a238629
aoelarsen <39989504+aoelarsen@users.noreply.github.com>
2022-12-31 13:14:39 +0100

    fixed a typo in the price list

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,8 @@ import preact from "@astrojs/preact";
 import image from "@astrojs/image";
 
 // https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact(), image()]
 });
