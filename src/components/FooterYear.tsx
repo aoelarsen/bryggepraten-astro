b1a3588df35b53ddca864753e22887db5b970aa5
aoelarsen <39989504+aoelarsen@users.noreply.github.com>
2023-01-02 17:19:53 +0100

    changed name on footer year component and export

---
+++ src/components/FooterYear.tsx
@@ -0,0 +1,8 @@
+const ThisYear = () => {
+  const year = new Date().getFullYear()
+
+  return <span>{year}</span>
+
+}
+
+export default ThisYear
--- src/components/thisYear.tsx
+++
@@ -1,7 +0,0 @@
-const year = new Date().getFullYear();
-
-const FooterYear = () => {
-  return <span>{year}</span>
-}
-
-export default FooterYear
